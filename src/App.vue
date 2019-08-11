f84954cd3677d8439127e78f8f52823f683b198f
che.lin <rockecstasy@yahoo.com.tw>
2019-08-11 22:41:13 +0800

    set App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
+    <PageFooter/>
   </div>
 </template>
 
 <script>
+// webpack.base.conf.js的resolve中， @: resolve('src')
+import PageHeader from '@/components/PageHeader'
+import PageFooter from '@/components/PageFooter'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    PageHeader,
+    PageFooter
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" src="./assets/style/main.scss"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import Comic from '@/pages/Comic'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/comic',
+    //   name: 'comic',
+    //   component: Comic
+    // },
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/comic',
+      name: 'comic',
+      // 會壓成webpack，適合大型專案
+      component: () => import('@/pages/Comic')
+    },
+    {
+      path: '/comic/:id',
+      name: 'comicDetail',
+      component: () => import('@/pages/comicDetail')
+    },
+    {
+      path: '/comic/:id/chapter/:cid',
+      name: 'comicChapter',
+      component: () => import('@/pages/comicChapter')
+    },
+    {
+      path: '*',
+      redirect: '/comic'
     }
   ]
 })
